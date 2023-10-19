8496f9ae6470e2e4d5d812b1de27c64171359e06
Christoph Rumpel <christoph@christoph-rumpel.com>
2023-10-19 13:51:36 +0200

    Video - Laravel Backup

--- config/filesystems.php
+++ config/filesystems.php
@@ -44,6 +44,13 @@ return [
             'throw' => false,
         ],
 
+        'backup' => [
+            'driver'   => 'ftp',
+            'host'     => 'nomoreencore.com',
+            'username' => env('BACKUP_FTP_USER'),
+            'password' => env('BACKUP_FTP_PW'),
+        ],
+
         's3' => [
             'driver' => 's3',
             'key' => env('AWS_ACCESS_KEY_ID'),
