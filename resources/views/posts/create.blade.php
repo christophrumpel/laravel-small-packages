2e8b37bf7138a92fe0806622146d3e8c5a9c4aff
Christoph Rumpel <christoph@christoph-rumpel.com>
2023-10-20 17:10:27 +0200

    Video - Blade UIkit (After)

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -10,42 +10,38 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
 
-                @if(session('alert'))
-                    <div role="alert" class="bg-green-700 text-green-100 p-4">
-                        {{ session('alert') }}
-                    </div>
-                @endif
+                <x-alert class="bg-green-700 text-green-100 p-4"/>
 
-                <form class="p-8" method="post" action="{{ route('create-post') }}">
-                    @csrf
+                <x-form class="p-8" action="{{ route('create-post') }}">
                     <div class="space-y-12">
                         <div class="border-b border-gray-900/10 pb-12">
                             <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
                                 <div class="sm:col-span-4">
-                                    <label for="title"
-                                           class="block text-sm font-medium leading-6 text-gray-900">Title</label>
+                                    <x-label for="title" class="block text-sm font-medium leading-6 text-gray-900"/>
+                                    <x-error field="title" class="text-red-500"/>
                                     <div class="mt-2">
                                         <div
                                             class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                                            <input type="text" name="title" id="title" autocomplete="username"
-                                                   class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-                                                   placeholder="My New Post...">
+                                            <x-input name="title" placeholder="My New Post..."
+                                                     class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"/>
                                         </div>
                                     </div>
                                 </div>
 
                                 <div class="col-span-full">
-                                    <label for="content"
-                                           class="block text-sm font-medium leading-6 text-gray-900">Content</label>
+                                    <x-label for="content" class="block text-sm font-medium leading-6 text-gray-900"/>
+                                    <x-error field="content" class="text-red-500"/>
                                     <div class="mt-2">
-                                        <textarea id="content" name="content" rows="3"
-                                                  class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></textarea>
+                                        <x-textarea name="content" rows="3"
+                                                    class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
                                     </div>
                                     <p class="mt-3 text-sm leading-6 text-gray-600">Write a few sentences about.</p>
                                 </div>
 
+
                                 <div class="sm:col-span-3">
-                                    <label for="country" class="block text-sm font-medium leading-6 text-gray-900">Category</label>
+                                    <x-label for="category" class="block text-sm font-medium leading-6 text-gray-900"/>
+                                    <x-error field="category" class="text-red-500"/>
                                     <div class="mt-2">
                                         <select id="category" name="category" autocomplete="category-name"
                                                 class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6">
@@ -73,8 +69,7 @@
                             Save
                         </button>
                     </div>
-                </form>
-
+                </x-form>
             </div>
         </div>
     </div>
